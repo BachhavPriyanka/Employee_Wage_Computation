52859d2d86a5a94a2de026551f1e5ff2e2394a0f
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-08-26 15:52:41 +0530

    Calculating using switch case

--- Emp_Welcome.java
+++ Emp_Welcome.java
@@ -5,31 +5,40 @@ package employeeWage;
 		public static void main(String[] args) {
 			System.out.println("Welcome to Employee Wage");
 			Emp_Welcome obj= new Emp_Welcome();
-			obj.CalEmployeeWage();
+			obj.Switch_case();
 		}
 		
-		public void CalEmployeeWage() {
-		    final int PART_TIME = 1;
-		    final int FULL_TIME = 2;
-		    final int Wage_Per_Hr = 20;
-		    int empType = (int) (Math.random() * 100) % 3;
-		    int workingHours = 0;
-		        if (empType == FULL_TIME)
-		    {
-		        System.out.println("Employee is Present Full Time");
-		        workingHours = 8;
-		    }
-		        else if (empType == PART_TIME)
+		public void Switch_case() {
+			  final int PART_TIME = 1;
+		        final int FULL_TIME = 2;
+		        final int Wage_Per_Hr = 20;
+		        final int Working_Days = 20;
+
+		        int totalWage = 0;
+		        for(int day=1;day<=Working_Days;day++)
 		        {
-		            System.out.println("Employee is Present Part Time");
-		            workingHours = 4;
+
+		            int empType = (int) (Math.random() * 100) % 3;
+		            int workingHours = 0;
+		            switch (empType)
+		            {
+		                case FULL_TIME:
+		                    workingHours = 8;
+		                    break;
+
+		                case PART_TIME:
+		                    workingHours = 4;
+		                    break;
+
+		                default:
+		            }
+
+		            int wage = workingHours * Wage_Per_Hr;
+		            System.out.println("Day " + day + " Wage is : " + wage);
+		            totalWage += wage;
+
 		        }
-		        else
-		    {
-		        System.out.println("Employee is Absent");
-		    }
-		    int wage = workingHours * Wage_Per_Hr;
-		        System.out.println("Employee Daily Wage is " + wage);
+		           System.out.println("Total wage for a month is " + totalWage);
 		    }
 		
 	}
