cbbe443a85291b2b81afe9d4d3170ae169bbe2fd
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-08-26 15:38:41 +0530

    Calculating Daily employee Wage

--- Emp_Welcome.java
+++ Emp_Welcome.java
@@ -5,19 +5,33 @@ package employeeWage;
 		public static void main(String[] args) {
 			System.out.println("Welcome to Employee Wage");
 			Emp_Welcome obj= new Emp_Welcome();
-			obj.Present_Absent();
+			obj.CalEmployeeWage();
 		}
 		
-		public void Present_Absent() {
-			final int FULL_TIME = 1;
-		    int empType = (int) (Math.random() * 100) % 2;
+		public void CalEmployeeWage() {
+		    final int PART_TIME = 1;
+		    final int FULL_TIME = 2;
+		    final int Wage_Per_Hr = 20;
+		    int empType = (int) (Math.random() * 100) % 3;
+		    int workingHours = 0;
 		        if (empType == FULL_TIME)
 		    {
-		        System.out.println("Employee is Present");
-		    } else
+		        System.out.println("Employee is Present Full Time");
+		        workingHours = 8;
+		    }
+		        else if (empType == PART_TIME)
+		        {
+		            System.out.println("Employee is Present Part Time");
+		            workingHours = 4;
+		        }
+		        else
 		    {
 		        System.out.println("Employee is Absent");
 		    }
+		    int wage = workingHours * Wage_Per_Hr;
+		        System.out.println("Employee Daily Wage is " + wage);
+		    }
+		
 	}
-}
+
 
