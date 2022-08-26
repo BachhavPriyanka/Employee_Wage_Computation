186f5a1cb7832b500141f97a9736cf018839274b
BachhavPriyanka <bachhavpriyanka09@gmail.com>
2022-08-26 15:29:27 +0530

    Employee present absent

--- Emp_Welcome.java
+++ Emp_Welcome.java
@@ -1,11 +1,23 @@
 package employeeWage;
 
-public class Emp_Welcome {
+	public class Emp_Welcome {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage");
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage");
+			Emp_Welcome obj= new Emp_Welcome();
+			obj.Present_Absent();
+		}
 		
+		public void Present_Absent() {
+			final int FULL_TIME = 1;
+		    int empType = (int) (Math.random() * 100) % 2;
+		        if (empType == FULL_TIME)
+		    {
+		        System.out.println("Employee is Present");
+		    } else
+		    {
+		        System.out.println("Employee is Absent");
+		    }
 	}
-	
 }
 
